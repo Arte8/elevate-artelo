16b6fd21483d6d58e3f538eb4b27d578d731b4fc
Art <artelo.studio@gmail.com>
2019-05-06 20:41:50 +0000

    added button, added form, good

--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -8,4 +8,23 @@ class TipsController < ApplicationController
   def new
     @tip = Tip.new
   end
+
+  def create
+    @tip = Tip.create(tip_params)
+    if @tip.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+
+    
+  end
+  
+  private
+
+  def tip_params
+    params.require(:tip).permit(:lesson, :translation)
+  end
+
+
 end
